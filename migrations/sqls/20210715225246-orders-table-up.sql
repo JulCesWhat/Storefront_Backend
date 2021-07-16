a5b5fc9f3791ef10f553b20b26486202820add0a
Julio Cesar Whatley <julceswhat@gmail.com>
2021-07-15 21:39:45 -0400

    Adding user db and handler endpoints

---
+++ migrations/sqls/20210715225246-orders-table-up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(250) NOT NULL,
+    userid VARCHAR(250) NOT NULL,
+    orderstatus VARCHAR(20) NOT NULL
+);
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import productRoutes from './handlers/product';
+import userRoutes from './handlers/user'
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address: string = "0.0.0.0:3000";
 
-app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', function (_req: Request, res: Response) {
     res.send('Hello World!')
-})
+});
+
+productRoutes(app);
+userRoutes(app);
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+    console.log(`starting app on: ${address}`);
+});
